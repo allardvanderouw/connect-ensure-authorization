890de8a4bbc8d60736a94fd64ed20a2201bda99c
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2018-12-01 16:27:43 +0100

    Update tests

--- test/index.js
+++
@@ -1,9 +0,0 @@
-// Unit tests
-require('./authorizer.unit.test.js');
-require('./ensure.unit.test.js');
-require('./index.unit.test.js');
-
-// Integration tests
-require('./ensure-default.integration.test.js');
-require('./ensure-custom.integration.test.js');
-require('./ensure-redirect.integration.test.js');
